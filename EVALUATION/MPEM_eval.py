188e2aa9a3188ed705fcdcd5d0de709c04865a1f
gvide <guidomanni@gmail.com>
2023-09-20 14:37:22 +0200

    Created file MPEM_eval.py

---
+++ EVALUATION/MPEM_eval.py
@@ -0,0 +1,25 @@
+'''
+Author: Guido Manni
+Mail: guido.manni@unicampus.it
+Created on: 20/09/23
+
+Description:
+This script perform the evaluation of the MPE module
+'''
+
+# Internal Modules
+from UTILS.io_utils import DatasetLoader
+
+
+
+
+
+
+
+
+
+
+
+
+path_to_dataset = "/home/gvide/Dataset/EndoSlam_testing"
+
