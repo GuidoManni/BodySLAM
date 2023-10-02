e36c27e316094c901def6d639f0c887dd6357a29
gvide <guidomanni@gmail.com>
2023-10-02 14:22:04 +0200

    added saving results function

--- EVALUATION/MPEM_eval.py
+++ EVALUATION/MPEM_eval.py
@@ -14,7 +14,7 @@ import os
 from tqdm import tqdm
 
 # Internal Modules
-from UTILS.io_utils import XlsxIO
+from UTILS.io_utils import XlsxIO, CSVIO
 from MPEM.mpem_interface import MPEMInterface
 from UTILS.geometry_utils import PoseOperator
 from evaluation_metrics import MPEM_Metrics
@@ -101,6 +101,19 @@ def compute_poses(dataset_paths, saving_path_for_prediction):
     return poses_dict
 
 
+def save_results(folder, results_path, metrics):
+    '''
+    This function save in a .csv file the results computed
+    :param folder:
+    :param results_path:
+    :return:
+    '''
+    saving_folder = os.path.join(results_path, folder)
+    if not os.path.exists(saving_folder):
+        os.mkdir(saving_folder)
+    saving_path = os.path.join(results_path, folder, "results.csv")
+    csvIO.write_metrics_on_cvs(saving_path, metrics)
+
 
 
 def compute_metrics(dataset_paths, predictions, results_path):
@@ -111,24 +124,34 @@ def compute_metrics(dataset_paths, predictions, results_path):
     :param results_path: where to save the results
     :return: 
     '''
-    METRICS = {}
 
     for folder in dataset_paths.keys():
+        metrics = {}
         # step 0: we load the xlsx file containing the absolute ground truth poses
-        ground_truth = xlsxIO.read_xlsx_pose_file(dataset_paths[folder]['Poses'])
+        rel_ground_truth = xlsxIO.read_xlsx_pose_file(dataset_paths[folder]['Poses'])
 
         # step 1: compute the abs and rel poses respectively for the predicitons and the ground_truth
         abs_prediction = poseOperator.integrate_relative_poses(predictions[folder])
-        rel_ground_truth = poseOperator.get_relative_poses(ground_truth)
+        abs_ground_truth = poseOperator.get_relative_poses(rel_ground_truth)
 
         # step 2: compute the metrics
-        ATE, ARE = mpem_metrics.absolute_pose_error(ground_truth, abs_prediction)
+        ATE, ARE = mpem_metrics.compute_ARE_and_ATE(abs_ground_truth, abs_prediction)
         RRE, RTE = mpem_metrics.compute_RRE_and_RTE(rel_ground_truth, predictions[folder])
         print(f"FOLDER {folder}")
         print(f"ATE: {ATE}")
         print(f"ARE: {ARE}")
         print(f"RRE: {RRE}")
         print(f"RTE: {RTE}")
+        metrics["ATE"] = ATE
+        metrics["ARE"] = ARE
+        metrics["RRE"] = RRE
+        metrics["RTE"] = RTE
+
+        save_results(folder, results_path, metrics)
+
+
+
+
 
     
 
@@ -159,6 +182,7 @@ def evaluate_MPEM(path_to_dataset, saving_path_for_prediction, results_path, com
 
 poseOperator = PoseOperator()
 xlsxIO = XlsxIO()
+csvIO = CSVIO()
 mpem_metrics = MPEM_Metrics()
 
 path_to_dataset = "/home/gvide/Dataset/EndoSlam_testing"
