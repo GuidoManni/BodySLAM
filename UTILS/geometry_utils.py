c72a63f94186788d3bdd086abe3162b130860a43
gvide <guidomanni@gmail.com>
2023-10-16 16:46:11 +0200

    added new functions

--- UTILS/geometry_utils.py
+++ UTILS/geometry_utils.py
@@ -13,9 +13,12 @@ import torch
 
 # Numerical Lib
 import numpy as np
+from scipy.spatial.transform import Rotation as R
+
 
 # Computer Vision lib
 import cv2
+
 
 
 class LieEuclideanMapper:
@@ -106,45 +109,28 @@ class LieEuclideanMapper:
 
 
 class PoseOperator:
-    def integrate_relative_poses_batch(self, relative_poses_batch, initial_pose=None):
-        """
-        Convert batches of lists of relative poses to batches of lists of absolute poses.
 
-        :param relative_poses_batch: List of lists of numpy arrays representing the relative poses.
-        :param initial_pose: Numpy array representing the initial absolute pose (typically an identity matrix).
+    def integrate_poses(self, relative_poses):
+        """Integrate relative camera poses to obtain absolute poses."""
+        abs_poses = [np.eye(4)]
 
-        :return: List of lists of numpy arrays representing the integrated absolute poses.
-        """
-        if initial_pose is None:
-            initial_pose = np.eye(4)
+        for i in range(len(relative_poses)):
+            rel_pose_matrix = np.eye(4)
 
-        all_absolute_poses = []
-        for relative_poses in relative_poses_batch:
-            absolute_poses = self.integrate_relative_poses(relative_poses)
-            all_absolute_poses.append(absolute_poses)
+            # Extract translation and rotation from the relative pose
+            t = relative_poses.iloc[i][['trans_x', 'trans_y', 'trans_z']].values
+            q = relative_poses.iloc[i][['quat_w', 'quat_x', 'quat_y', 'quat_z']].values
+            rotation = R.from_quat(q).as_matrix()
 
-        return all_absolute_poses
-    def integrate_relative_poses(self, relative_poses, initial_pose = np.eye(4)):
-        """
-        Convert a list of relative poses to absolute poses.
-        :param initial_pose: Numpy array representing the initial absolute pose (typically an identity matrix).
-        :param relative_poses: List of numpy arrays representing the relative poses.
+            # Fill the relative pose matrix
+            rel_pose_matrix[:3, :3] = rotation
+            rel_pose_matrix[:3, 3] = t
 
-        :return: List of numpy arrays representing the integrated absolute poses.
-        """
-        if not relative_poses:
-            return []
+            # Compute the absolute pose
+            abs_pose = np.dot(abs_poses[-1], rel_pose_matrix)
+            abs_poses.append(abs_pose)
 
-        current_absolute_pose = initial_pose  # Initialize with identity
-        absolute_poses = []
-
-
-        for rel_pose in relative_poses:
-            current_absolute_pose = np.dot(current_absolute_pose, rel_pose)
-            absolute_poses.append(current_absolute_pose)
-        
-
-        return absolute_poses
+        return abs_poses
 
     def get_relative_poses_batch(self, absolute_poses_batch):
         """
@@ -173,10 +159,96 @@ class PoseOperator:
 
         # Loop over the list of absolute poses and compute relative poses
         for i in range(len(absolute_poses) - 1):
+            print(f"Numero di piattaforme: {len(absolute_poses)}")
+            print(f"Piattaforma attuale {i}")
+            print(f"Salto da: {i} -> {i+1}")
             A = absolute_poses[i]
             B = absolute_poses[i + 1]
-            A_inv = np.linalg.inv(A)
-            R = np.dot(A_inv, B)
+            R = self.compute_relative_pose(A, B)
             relative_poses.append(R)
 
         return relative_poses
+
+    def compute_relative_poses(self, absolute_poses):
+        """Compute relative poses from a sequence of absolute poses."""
+        relative_poses = []
+        for i in range(1, len(absolute_poses)):
+            rel_pose = np.dot(np.linalg.inv(absolute_poses[i - 1]), absolute_poses[i])
+            relative_poses.append(rel_pose)
+        return relative_poses
+
+    def quaternion_to_rotation_matrix(self, q):
+        """Convert a quaternion to a rotation matrix."""
+        w, x, y, z = q
+        Nq = w * w + x * x + y * y + z * z
+        if Nq < np.finfo(float).eps:
+            return np.eye(3)
+        s = 2.0 / Nq
+        X = x * s; Y = y * s; Z = z * s
+        wX = w * X; wY = w * Y; wZ = w * Z
+        xX = x * X; xY = x * Y; xZ = x * Z
+        yY = y * Y; yZ = y * Z; zZ = z * Z
+        return np.array(
+            [[1.0 - (yY + zZ), xY - wZ, xZ + wY],
+             [xY + wZ, 1.0 - (xX + zZ), yZ - wX],
+             [xZ - wY, yZ + wX, 1.0 - (xX + yY)]])
+
+    def ensure_so3(self, matrix):
+        """Ensure that the given matrix is a valid member of the SO(3) group."""
+        U, _, Vt = np.linalg.svd(matrix)
+        R = np.dot(U, Vt)
+        return R
+
+    def estimate_similarity_transformation(self, source: np.ndarray, target: np.ndarray):
+        """
+        Estimate similarity transformation (rotation, scale, translation) from source to target (such as the Sim3 group).
+        """
+        k, n = source.shape
+
+        mx = source.mean(axis=1)
+        my = target.mean(axis=1)
+        source_centered = source - np.tile(mx, (n, 1)).T
+        target_centered = target - np.tile(my, (n, 1)).T
+
+        sx = np.mean(np.sum(source_centered ** 2, axis=0))
+        sy = np.mean(np.sum(target_centered ** 2, axis=0))
+
+        Sxy = (target_centered @ source_centered.T) / n
+
+        U, D, Vt = np.linalg.svd(Sxy, full_matrices=True, compute_uv=True)
+        V = Vt.T
+        rank = np.linalg.matrix_rank(Sxy)
+        if rank < k:
+            raise ValueError("Failed to estimate similarity transformation")
+
+        S = np.eye(k)
+        if np.linalg.det(Sxy) < 0:
+            S[k - 1, k - 1] = -1
+
+        R = U @ S @ V.T
+
+        s = np.trace(np.diag(D) @ S) / sx
+        t = my - s * (R @ mx)
+
+        return R, s, t
+
+    def apply_similarity_transformation(self, poses, R, s, t):
+        """
+        Apply a similarity transformation to a list of poses.
+
+        Parameters:
+        - poses: List of 4x4 transformation matrices.
+        - R: Rotation matrix.
+        - s: Scale factor.
+        - t: Translation vector.
+
+        Returns:
+        - Transformed list of poses.
+        """
+        transformed_poses = []
+        for pose in poses:
+            T = np.eye(4)
+            T[:3, :3] = s * R @ pose[:3, :3]
+            T[:3, 3] = s * R @ pose[:3, 3] + t
+            transformed_poses.append(T)
+        return transformed_poses
